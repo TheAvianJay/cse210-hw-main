6b6eaaf77b16db4732d6f7f6948c8550bab3de63
jrpgaming101@gmail.com <jrpgaming101@gmail.com>
2023-11-23 10:30:41 -0700

    memes

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        
+        Assignment a1 = new Assignment("Riki Rakito", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        Mathassignment a2 = new Mathassignment("Sam Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Scarlet Red", "European History", "The Causes of World War II where an Austrian painter thought it was a great idea to invade the Soviet Union");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
